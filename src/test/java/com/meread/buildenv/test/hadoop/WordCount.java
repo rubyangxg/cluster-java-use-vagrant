ae06b7f31378a38b23d6a04c7d8c9775ce6e2848
yangxiangguo <xiangguo.yang@gmail.com>
2016-02-02 16:00:43 +0800

    add ssh example

--- src/test/java/com/meread/buildenv/test/hadoop/WordCount.java
+++ src/test/java/com/meread/buildenv/test/hadoop/WordCount.java
@@ -1,12 +1,6 @@
 package com.meread.buildenv.test.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class WordCount {
@@ -18,15 +12,15 @@ public class WordCount {
             System.exit(2);
         }
 
-        Job job = new Job(conf, "wordcount");
-        job.setJarByClass(WordCount.class);
-        job.setMapperClass(TokenizerMapper.class);
-        job.setCombinerClass(IntSumReducer.class);
-        job.setReducerClass(IntSumReducer.class);
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(IntWritable.class);
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+//        Job job = new Job(conf, "wordcount");
+//        job.setJarByClass(WordCount.class);
+//        job.setMapperClass(TokenizerMapper.class);
+//        job.setCombinerClass(IntSumReducer.class);
+//        job.setReducerClass(IntSumReducer.class);
+//        job.setOutputKeyClass(Text.class);
+//        job.setOutputValueClass(IntWritable.class);
+//        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+//        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+//        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }

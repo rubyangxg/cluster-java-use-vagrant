09dade594567d739e0c4410a35806a2658176bae
yangxiangguo <xiangguo.yang@gmail.com>
2016-02-03 12:32:41 +0800

    add ssh example

--- src/test/java/com/meread/buildenv/test/hadoop/HadoopTest.java
+++ src/test/java/com/meread/buildenv/test/hadoop/HadoopTest.java
@@ -1,6 +1,7 @@
 package com.meread.buildenv.test.hadoop;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileStatus;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.junit.After;
@@ -18,27 +19,45 @@ public class HadoopTest {
 
     private static final String hdfsUrl = "hdfs://node1:9000";
 
-//    private DFSClient client;
-
-    private FileSystem fileSystem;
+    private FileSystem fs;
 
     @Before
     public void getConf() throws URISyntaxException, IOException {
         Configuration conf = new Configuration();
         URI uri = new URI(hdfsUrl);
-//        client = new DFSClient(uri, conf);
-        fileSystem = FileSystem.get(uri, conf);
+        fs = FileSystem.get(uri, conf);
     }
 
     @After
     public void closeClient() throws IOException {
-        fileSystem.close();
+        fs.close();
     }
 
     @Test
     public void uploadFromLocal() throws URISyntaxException, IOException {
-        Path localFile = new Path("/Users/yangxg/cluster-java-use-vagrant/src/test/resources/apache.license.txt");
+        String file = getClass().getClassLoader().getResource("apache.license.txt").getFile();
+        Path localFile = new Path(file);
         Path destFile = new Path("/apache.license.txt");
-        fileSystem.copyFromLocalFile(false, true, localFile, destFile);
+        fs.copyFromLocalFile(false, true, localFile, destFile);
     }
+
+    @Test
+    public void del() throws URISyntaxException, IOException {
+        Path destFile = new Path("/apache.license.txt");
+        fs.delete(destFile, false);
+    }
+
+    @Test
+    public void ls() throws URISyntaxException, IOException {
+        Path dir = new Path("/");
+
+        FileStatus[] list = fs.listStatus(dir);
+        System.out.println("ls: /");
+        System.out.println("==========================================================");
+        for (FileStatus f : list) {
+            System.out.printf("name: %s, folder: %s, size: %d\n", f.getPath(), f.isDirectory(), f.getLen());
+        }
+        System.out.println("==========================================================");
+    }
+
 }

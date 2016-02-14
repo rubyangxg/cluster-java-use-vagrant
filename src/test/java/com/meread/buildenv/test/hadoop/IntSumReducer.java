689e57e9b029b233d62366b0625aedf7aead54bd
yangxiangguo <xiangguo.yang@gmail.com>
2016-02-14 16:39:46 +0800

    add word count example

--- src/test/java/com/meread/buildenv/test/hadoop/IntSumReducer.java
+++ src/test/java/com/meread/buildenv/test/hadoop/IntSumReducer.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class IntSumReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
     IntWritable result = new IntWritable();
 
-    public void reduce(Text key, Iterable<IntWritable> values, Mapper.Context context) throws IOException, InterruptedException {
+    protected void reduce(Text key, Iterable<IntWritable> values, Mapper.Context context) throws IOException, InterruptedException {
         //输入参数key为单个单词;
         //输入参数Iterable<IntWritable> values为各个Mapper上对应单词的计数值所组成的列表。
         int sum = 0;

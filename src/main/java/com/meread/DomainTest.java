37fa905da4cf1f32ad8cdd93f08e8b2a72478691
yangxiangguo <xiangguo.yang@gmail.com>
2016-02-18 17:40:32 +0800

    add word count example

--- src/main/java/com/meread/DomainTest.java
+++ src/main/java/com/meread/DomainTest.java
@@ -3,12 +3,15 @@ package com.meread;
 import com.google.common.collect.Lists;
 import org.apache.commons.io.FileUtils;
 import org.apache.http.client.fluent.Request;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by yangxg on 16/2/18.
@@ -16,7 +19,10 @@ import java.util.concurrent.Executors;
 public class DomainTest {
     static int processed = 0;
 
-    public static void main(String[] args) throws IOException {
+    private static final Logger logger = LoggerFactory.getLogger(DomainTest.class);
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        System.out.println("start..");
         File domain_result = new File(DomainTest.class.getClassLoader().getResource("tmp/domain_result.txt").getFile());
         final File result = new File(DomainTest.class.getClassLoader().getResource("tmp/result.txt").getFile());
         FileUtils.write(result, "", false);
@@ -40,7 +46,7 @@ public class DomainTest {
 //            List<String> next = ci.next();
 //            FileUtils.write(domain_result, next.get(0) + next.get(1) + ".com" + System.getProperty("line.separator"), true);
 //        }
-        int threadSize = 10;
+        int threadSize = 2;
 
         final List<String> lines = FileUtils.readLines(domain_result);
         final int total = lines.size();
@@ -50,28 +56,25 @@ public class DomainTest {
             es.execute(new Runnable() {
                 @Override
                 public void run() {
-                    StringBuilder sb = new StringBuilder(50);
-                    for (int i = 0; i < process.size(); i++) {
-                        String s = process.get(i);
-                        sb.append(s).append(",");
-                        if (i != 0 && i % 10 == 0) {
-                            String url = "http://panda.www.net.cn/cgi-bin/check.cgi?area_domain=" + sb;
-                            String response = null;
-                            try {
-                                response = Request.Get(url).execute().returnContent().asString();
-                                FileUtils.write(result, response + System.getProperty("line.separator"), true);
-                                sb = new StringBuilder(50);
-                                processed += 10;
-                                System.out.println(total + ":" + processed + " : " + response);
-                            } catch (IOException e) {
-                                e.printStackTrace();
-                            }
+                    List<List<String>> subProcesses = Lists.partition(process, 10);
+                    for (List<String> subProcess : subProcesses) {
+                        StringBuilder sb = new StringBuilder(50);
+                        for (String s : subProcess) {
+                            sb.append(s).append(",");
+                        }
+                        String url = "http://panda.www.net.cn/cgi-bin/check.cgi?area_domain=" + sb;
+                        try {
+                            String response = Request.Get(url).execute().returnContent().asString();
+                            FileUtils.write(result, response + System.getProperty("line.separator"), true);
+                            processed += 10;
+                            logger.info(total + ":" + processed + " : " + response);
+                        } catch (IOException e) {
+                            e.printStackTrace();
                         }
                     }
                 }
             });
         }
-
-
+        es.awaitTermination(10, TimeUnit.DAYS);
     }
 }
